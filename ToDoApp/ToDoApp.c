31cd3c56a51a622783e3bdf196c5f02290e470ec
sm9026 <cauchyschwarz1821@gmail.com>
2025-07-01 23:15:45 +0530

    added task status

--- ToDoApp/ToDoApp.c
+++ ToDoApp/ToDoApp.c
@@ -97,6 +97,31 @@ void addTask()
 
     fprintf(file, "%d. Task: %s\n...Description: %s\n...Priority level: %s\n", iD, name, description, priority);
     fclose(file);
+    
+    // setting all tasks as not done in NotDone.txt
+    file = fopen("NotDone.txt", "r");
+    if (file == NULL){
+        file = fopen("NotDone.txt", "w");
+        if (file == NULL)
+        {
+            printf("Error creating NotDone.txt file!\n");
+            return;
+        }
+        fprintf(file, "TASK STATUS: NOT DONE:\n");
+    }
+    else
+    {
+        fclose(file);
+        file = fopen("NotDone.txt", "a");
+        if (file == NULL)
+        {
+            printf("Error opening NotDone.txt for appending!\n");
+            return;
+        }
+
+    }
+    fprintf(file, "%d. Task: %s\n...Description: %s\n...Priority level: %s\n", iD, name, description, priority);
+    fclose(file);
 }
 
 // 02 viewTasks function
@@ -158,7 +183,7 @@ void deleteTask()
         return;
     }
 
-    //int lc = countLinesInFile("tasks.txt");
+    // Read all lines from the file into an array
     int i = 0;
     char lines[100][250];
     while (fgets(lines[i], sizeof(lines[i]), temp) && i < l)
@@ -167,22 +192,7 @@ void deleteTask()
     }
     fclose(temp);
 
-  /* FILE *tem = fopen("temp.txt", "w");
-    if (temp == NULL)
-    {
-        printf("Error opening temporary file for writing!\n");
-        return;
-    }
-    for (i = 0; i < l; i++){
-        for (int j = 0; j < strlen(lines[i]); j++)
-        {
-            fprintf(tem, "%c", lines[i][j]);
-    }
-
-}
-fclose(tem);*/
-
-    
+    // Reopen the file in write mode to overwrite it without the deleted task
     FILE *file = fopen("tasks.txt", "w");
     if (file == NULL)
     {
@@ -309,6 +319,167 @@ fclose(tem);*/
 }
 
 
+// 05 change task status
+void changeTaskStatus()
+{
+    //
+    int totalTasks = (countLinesInFile("NotDone.txt")-1) / 3; // Calculate total tasks based on the number of lines
+    clearInputBuffer();
+    int toChange;
+    printf("Enter the task number to change: ");
+    scanf("%d", &toChange);
+    clearInputBuffer();
+    if (toChange < 1 || toChange > totalTasks)
+    {
+        printf("Invalid task number. Please try again.\n");
+        clearInputBuffer();
+        return;
+    }
+    FILE *temp = fopen("NotDone.txt", "r");
+    if (temp == NULL)
+    {
+        printf("No tasks found.\n");
+        return;
+    }
+
+    // Read all lines from the file into an array
+    int i = 0;
+    char lines[100][250];
+    while (fgets(lines[i], sizeof(lines[i]), temp) && i < l)
+    {
+        i++;
+    }
+    fclose(temp);
+
+    int lc = countLinesInFile("NotDone.txt");
+
+    FILE *file = fopen("NotDone.txt", "w");
+    if (file == NULL)
+    {
+        printf("Error opening file for writing!\n");
+        return;
+    }
+    fprintf(file, "TASK STATUS: NOT DONE:\n");
+    fclose(file);
+
+    i = 1;
+    int j;
+    char tsk[100];
+    char desc[150];
+    char prio[100]; 
+    int iD = 1;
+
+    while (i < lc){
+
+        if ((i - 1)/3 + 1 == toChange)
+        {
+            FILE *done = fopen("Done.txt", "r");
+            if (done == NULL)
+            {
+                // If Done.txt doesn't exist, create it
+                done = fopen("Done.txt", "w");
+                if (done == NULL)
+                {
+                    printf("Error creating bin file!\n");
+                    return;
+                }
+                fprintf(done, "TASK STATUS: DONE:\n");
+            }
+            else
+            {
+                fclose(done); // Close the file to reopen in append mode later
+                done = fopen("Done.txt", "a");
+                if (done == NULL)
+                {
+                    printf("Error opening bin file for appending!\n");
+                    return;
+                }
+            }
+
+            //storing done task name
+            for (j = 0; lines[i][j] != '\0'; j++)
+            {
+                tsk[j] = lines[i][j + 3];
+            }
+            tsk[j] = '\0'; // Null-terminate the string
+            i++;
+            
+            //storing done task description
+            for (j = 0; lines[i][j] != '\0'; j++)
+            {
+                desc[j] = lines[i][j];
+            }
+            desc[j] = '\0'; // Null-terminate the string
+            i++;
+            
+            //storing deleted task priority
+            for (j = 0; lines[i][j] != '\0'; j++)
+            {
+                prio[j] = lines[i][j];
+            }
+            prio[j] = '\0'; // Null-terminate the string
+            i++;
+
+            
+            int DiD;
+            int lineCount = countLinesInFile("bin.txt");
+            if (lineCount == 0)
+            {
+                DiD = 1;
+            }
+            else
+            {
+                DiD = lineCount / 3 + 1;
+            }
+            
+            fprintf(done, "%d. %s%s%s", DiD, tsk, desc, prio); // add deleted task to Done.txt
+            fclose(done);
+            continue;
+        }
+
+        else{
+        FILE *file = fopen("NotDone.txt", "a");
+        if (file == NULL)
+        {
+            printf("Error opening file for writing!\n");
+            return;
+        }          
+        //storing task name
+        for (j = 0; lines[i][j] != '\0'; j++)            
+        {
+            tsk[j] = lines[i][j + 3];
+        }
+        tsk[j] = '\0'; // Null-terminate the string
+        i++;
+        
+        //storing description
+        for (j = 0; lines[i][j] != '\0'; j++)          
+        {
+            desc[j] = lines[i][j];
+        }
+        desc[j] = '\0'; // Null-terminate the string
+        i++;
+        
+        //storing priority
+        for (j = 0; lines[i][j] != '\0'; j++)      
+        {
+            prio[j] = lines[i][j];
+        }
+        prio[j] = '\0'; // Null-terminate the string
+        i++;
+        fprintf(file, "%d. %s%s%s", iD, tsk, desc, prio);
+        iD++;
+        fclose(file);
+        }
+
+
+
+
+    }
+    
+}
+
+
 
 
 // main function
@@ -342,6 +513,8 @@ int main()
             printf("type 'view all tasks' to view all tasks\n");
             printf("type 'delete' to delete a task\n");
             printf("type 'view bin' to view deleted tasks\n");
+            printf("type 'change status' to change task status to done\n");
+            printf("type 'check' to check status of tasks\n");
             printf("=========================================================\n");
 
             char command[20];
@@ -389,6 +562,51 @@ int main()
                 viewTasks(binFile);
                 printf("\nNo of deleted tasks: %d", (countLinesInFile("bin.txt")-1)/3);
                 fclose(binFile);
+            }
+            else if (strcmp(command, "change status") == 0)
+            {
+                changeTaskStatus();
+                printf("Task status changed successfully!\n");
+            }
+
+            else if (strcmp(command, "check") == 0)
+            {
+                printf("Check staus type (Done/Not Done): ");
+                char status[20];
+                if (fgets(status, sizeof(status), stdin) == NULL)
+                {
+                    printf("Error reading status!\n");
+                    continue;
+                }
+                
+                if (strcmp(status, "Done\n") == 0)
+                {
+                    FILE *doneFile = fopen("Done.txt", "r");
+                    if (doneFile == NULL)
+                    {
+                        printf("No tasks marked as done.\n");
+                        continue;
+                    }
+                    viewTasks(doneFile);
+                    printf("\nNo of tasks done: %d", (countLinesInFile("Done.txt")-1)/3);
+                    fclose(doneFile);
+                }
+                else if (strcmp(status, "Not Done\n") == 0)
+                {
+                    FILE *notDoneFile = fopen("NotDone.txt", "r");
+                    if (notDoneFile == NULL || countLinesInFile("NotDone.txt") <= 1)
+                    {
+                        printf("No tasks pending.\n");
+                        continue;
+                    }
+                    viewTasks(notDoneFile);
+                    printf("\nNo of tasks pending: %d", (countLinesInFile("NotDone.txt")-1)/3);
+                    fclose(notDoneFile);
+                }
+                else
+                {
+                    printf("Invalid status type. Please enter 'Done' or 'Not Done'.\n");
+                }
             }
 
             else
